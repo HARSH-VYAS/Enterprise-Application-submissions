95d9cf613fc21e44b4a49473f9ef2c2b3e1d57e2
HARSH-VYAS <harshvyas1412@gmail.com>
2015-03-29 14:14:21 -0700

    Cmpe273Asssignment2

---
+++ assignment1/src/main/java/hello/moderatorRepository.java
@@ -0,0 +1,12 @@
+package hello;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface moderatorRepository extends MongoRepository<Moderator,String> {
+
+	
+	
+	
+	
+	
+}
